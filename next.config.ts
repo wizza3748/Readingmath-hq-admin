ea38ea1d7a1a0fdd1aab7f2ef4e2169720173282
wizza <wizza3208@gmail.com>
2025-12-31 01:34:56 +0000

    Try fixing this error: `Runtime Error: Invalid src prop (https://raw.git

--- next.config.ts
+++ next.config.ts
@@ -1,3 +1,4 @@
+
 import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
@@ -31,6 +32,12 @@ const nextConfig: NextConfig = {
       {
         protocol: 'https',
         hostname: 'reading-math.s3.ap-northeast-2.amazonaws.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'raw.githubusercontent.com',
         port: '',
         pathname: '/**',
       }
